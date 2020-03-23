e13eeecda7e223d9193be6ea803d666ba138d531
feverton <feverton@gmail.com>
2020-03-22 23:56:18 -0300

    Atualiza base

--- documentos/base_igreja.sql
+++ documentos/base_igreja.sql
@@ -4,9 +4,37 @@ CREATE DATABASE `igreja`;
 
 USE `igreja`;
 
-CREATE TABLE `enderecos` () ENGINE=INNODB;
+CREATE TABLE `correios` (
+	`id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	`cep` CHAR(8) UNIQUE,
+	`logradouro` VARCHAR(64),
+	`bairro` VARCHAR(64),
+	`localidade` VARCHAR(64),
+	`uf` CHAR(2)
+) ENGINE=INNODB;
 
-CREATE TABLE `tipos_telefones` () ENGINE=INNODB;
+CREATE TABLE `tipos_enderecos` (
+	`id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	`descricao` VARCHAR(32)
+) ENGINE=INNODB;
+
+INSERT INTO `tipos_enderecos` VALUES (1,'CASA'),(2,'LOTE'),(3,'APARTAMENTO');
+
+CREATE TABLE `enderecos` (
+	`id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	`endereco_id` INT,
+	`tipoendereco_id` INT,
+	`numero` VARCHAR(16),
+	FOREIGN KEY (`endereco_id`) REFERENCES `enderecos`(`id`)
+	ON UPDATE CASCADE ON DELETE RESTRICT,
+	FOREIGN KEY (`tipoendereco_id`) REFERENCES `tipos_enderecos`(`id`)
+	ON UPDATE CASCADE ON DELETE RESTRICT
+) ENGINE=INNODB;
+
+CREATE TABLE `tipos_telefones` (
+	`id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	`descricao` VARCHAR(32)
+) ENGINE=INNODB;
 
 CREATE TABLE `telefones` () ENGINE=INNODB;
 
